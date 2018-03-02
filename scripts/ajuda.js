76030549b5dab81928a6f97b43e0d2f917092f95
Joao Fabio Vicente <joaofabiov@gmail.com>
2018-03-02 11:37:23 +0000

    adiciona features de cinema, ajuda e regras

---
+++ scripts/ajuda.js
@@ -0,0 +1,28 @@
+//  Description:
+//    Checks the behavior of the Agile Promoters
+//  Commands:
+
+//    fala bixo
+
+// o que está ouvindo
+const COMMANDS = {
+  HELLO: /ajuda/i,
+};
+
+// Lógica da ação do bot
+const _msgInicial = (msg) => {
+  msg.reply(`Comandos:
+  	*trailer [nome do filme]* : Retorna trailer do Filme
+  	*informe as regras* : Retorna as regras do clã
+  	*informe o link* : Retorna o link com a planilha do clã`);
+}
+
+
+// Como e o que a gente passa para a função
+const _saudacao = msg => _msgInicial(msg);
+
+//const _link = msgLink => _msgLink(msgLink);
+
+module.exports = (robot) => {
+  robot.respond(COMMANDS.HELLO, _saudacao);
+};
