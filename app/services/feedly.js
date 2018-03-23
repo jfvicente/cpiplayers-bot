938ae4d20a220693034a05489e2bd8718e7c6283
Leonardo Oliveira <leonardo.oliveira@superprix.com.br>
2018-03-22 21:37:36 -0300

    api key do feedly na variavel de ambiente

--- app/services/feedly.js
+++ app/services/feedly.js
@@ -1,4 +1,4 @@
-const ACCESS_TOKEN = "A1szYsVI2yswz7-9e2pU0QMh85pSDYrjC88xkBE0l-GfXDQ6Vt_94Tb7aLAq6Ho4SE-ok_iyEekR0I8qjhp-bPDhnYf0k_fj-R-ilAKQDArxqCOfRGTFmjkNrk3-bvKvCT3bwrCrglqbff5U7-3iroKKsJKHR7kM7jC1j2SW2OBg-GGWmDiqxv0z7p41HSQEFOHNaA1T6RpTp9fOAq43YWntU8JDJtOVl3ckZwqSdZaMkC7vPrTWP2zj4BSvxPs:feedly";
+const ACCESS_TOKEN = process.env.FEEDLY_ACCESS_TOKEN;
 const STREAM_ID = "user/c7e22790-a4cb-46f4-b602-06117af18929/category/Games";
 const URL = `https://cloud.feedly.com/v3/streams/contents?streamId=${STREAM_ID}&count=5`;
 
