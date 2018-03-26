4cb6963d7d29a79ebb0df6afdd620a34d7bd9277
Leonardo Oliveira <leonardo.oliveira@superprix.com.br>
2018-03-26 16:50:11 -0300

    inclusão do ouvinte do mixer para notificar quando um membro está transmitindo. Melhoria da formatação da ajuda

--- scripts/ajuda.js
+++ scripts/ajuda.js
@@ -1,35 +1,74 @@
 //  Description:
-//    Checks the behavior of the Agile Promoters
-//  Commands:
-
-//    fala bixo
-
-// o que está ouvindo
-const COMMANDS = {
-  HELLO: /ajuda/i,
-};
-
-// Lógica da ação do bot
-const _msgInicial = (msg) => {
-	msg.reply(`Comandos:
-		*trailer [nome do filme]* : Retorna trailer do Filme
-		*informe as regras* : Retorna as regras do clã
-		*informe o link* : Retorna o link com a planilha do clã
-		*passo a passo slack* : Retorna o link do PDF com o passo a passo
-		*noticias* : Retorna as ultimas 5 noticias sobre games
-		*meu rank bf1* : mostra a sua posição no rank bf1 geral e nas classes
-		*rank bf1 [classe ou geral]* : mostra todo o rank bf1 geral ou da classe [assalto | medico | suporte | batedor | cavalaria | piloto | tanqueiro]
-		*rank pubg* : mostra o rank CPI do PUBG
-		*stats pubg* : mostra suas estatisticas no PUBG
-		*meu clipe xbox* : mostra a ultima captura de video do xbox`);
-}
-
-
-// Como e o que a gente passa para a função
-const _saudacao = msg => _msgInicial(msg);
-
-//const _link = msgLink => _msgLink(msgLink);
+//    Mostra os comandos que o @c-3po suporta
+//  Commands: 
+// 		@c-3po ajuda
 
 module.exports = (robot) => {
-  robot.respond(COMMANDS.HELLO, _saudacao);
+  robot.respond(/ajuda/i, (msg) =>{
+
+		var comands = [
+				{
+						"color": "#36a64f",                  
+						"title": "Mostra o ultimo trailer do filme consultado no youtube",           
+						"text": "`@c-3po trailer [nome_do_filme]`\nExemplo: `@c-3po trailer vingadores guerra infinita`",
+						"mrkdwn_in": ["text", "pretext"]
+				},
+				{
+						"color": "#36a64f",                  
+						"title": "Mostra as regras basicas do clã",           
+						"text": "`@c-3po informe as regras`",
+						"mrkdwn_in": ["text", "pretext"]
+				},
+				{
+						"color": "#36a64f",                  
+						"title": "Mostra a planilha com todos os membros do clã",           
+						"text": "`@c-3po informe o link`",
+						"mrkdwn_in": ["text", "pretext"]
+				},
+				{
+						"color": "#36a64f",                  
+						"title": "Mostra o link do PDF com as principais funções do SLACK",           
+						"text": "`@c-3po passo a passo slack`",
+						"mrkdwn_in": ["text", "pretext"]
+				},
+				{
+						"color": "#36a64f",                  
+						"title": "Mostra as 5 últimas noticias sobre games",           
+						"text": "`@c-3po noticias`",
+						"mrkdwn_in": ["text", "pretext"]
+				},
+				{
+						"color": "#36a64f",                  
+						"title": "Mostra a sua posição no RANK CPI - BF1",           
+						"text": "`@c-3po meu rank bf1`",
+						"mrkdwn_in": ["text", "pretext"]
+				},
+				{
+						"color": "#36a64f",                  
+						"title": "Mostra o RANK CPI - BF1",           
+						"text": "`@c-3po rank bf1 [nome_classe ou geral]`\nnome_classe = [assalto | medico | suporte | batedor | cavalaria | piloto | tanqueiro]\nExemplo:`@c-3po rank bf1 geral` ou `@c-3po rank bf1 assalto`",
+						"mrkdwn_in": ["text", "pretext"]
+				},
+				{
+						"color": "#36a64f",                  
+						"title": "Mostra o RANK CPI - PUBG",           
+						"text": "`@c-3po rank pubg`",
+						"mrkdwn_in": ["text", "pretext"]
+				},
+				{
+						"color": "#36a64f",                  
+						"title": "Mostra suas estatisticas no PUBG",           
+						"text": "`@c-3po stats pubg`",
+						"mrkdwn_in": ["text", "pretext"]
+				},
+				{
+						"color": "#36a64f",                  
+						"title": "Mostra o último clipe do xbox",           
+						"text": "`@c-3po meu clipe xbox`",
+						"mrkdwn_in": ["text", "pretext"]
+				}				
+			];
+
+		robot.adapter.client.web.chat.postMessage(msg.message.room, "Aqui estão todos os comandos que você pode usar:", {as_user:true, attachments: comands});
+	});
 };
---
+++ scripts/mixer.js
@@ -0,0 +1,46 @@
+// Description:
+//   Mostra quando um membro começa uma transmissão no mixer
+//
+
+var data = require("../app/data/cpiplayers.json");
+const Carina = require('carina').Carina;
+const ws = require('ws');
+const https = require('https');
+
+Carina.WebSocket = ws;
+const ca = new Carina({ isBot: true }).open();
+const channelName = 'c-3po-learning'; 
+
+process.removeAllListeners();
+ca.setMaxListeners(Infinity);
+process.setMaxListeners(Infinity);
+
+module.exports = (robot) =>{        
+    //let i = 0;
+    data.filter(x => x.Status == "CPI").forEach((membro, idx, arr) => {
+        let cpitag = membro.Gamer_Tag;
+        let URL = `https://mixer.com/api/v1/channels/${cpitag.replace(" ", "_")}?fields=id`;
+        https.get(URL, (response) =>{
+            let rawData = '';
+            response.on('data', (chunk) => { rawData += chunk; });
+
+            response.on('end', () => {            
+                if(response.statusCode == 200){
+                    var obj = JSON.parse(rawData);
+                    if(obj){
+                        ca.subscribe(`channel:${obj.id}:update`, data => {
+                            //console.log(data);
+                            if(data.online != undefined)
+                                if(data.online)
+                                    robot.messageRoom(channelName, `\nO Mestre ${cpitag} está fazendo uma transmissão pelo Mixer, acompanhem!!!\n<https://mixer.com/${cpitag.replace(" ", "_")}|https://mixer.com/${cpitag.replace(" ", "_")}>`);
+                                
+                                if(!data.online)
+                                    robot.messageRoom(channelName, `:dissapointed: Ah! que pena... ${cpitag} terminou a transmissão no Mixer. Mas vocês podem ver as ultimas transmissões gravadas.\n<https://mixer.com/${cpitag.replace(" ", "_")}|https://mixer.com/${cpitag.replace(" ", "_")}>`);
+                            });
+                    }
+                }
+            });
+        }).on('error', error => console.log(error));
+    })
+
+}
